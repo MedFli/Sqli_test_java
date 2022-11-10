e20bfc1119ea9e01a7b9813f8f18ba20d1e81abf
MedFli <flissate.amine@gmail.com>
2022-11-10 15:25:49 +0100

    first commit

--- coin/src/main/java/com/sqli/coin/model/CoinMachine.java
+++ coin/src/main/java/com/sqli/coin/model/CoinMachine.java
@@ -4,12 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CoinMachine {
-     double total;
-     int valueOfCoins;
-     int numberOfCoins;
-
-     int numberOf50;
-     int numberOf20;
+    double total;
+    int valueOfCoins;
+    int numberOfCoins;
+    int numberOf50;
+    int numberOf20;
 
     public CoinMachine() {
     }
@@ -22,7 +21,7 @@ public class CoinMachine {
         return total;
     }
 
-    public  void putMoneyInside(String s) {
+    public void putMoneyInside(String s) {
         numberOfCoins = Integer.parseInt(s.substring(0, s.indexOf("x")));
         valueOfCoins = Integer.parseInt(s.substring(s.indexOf("x") + 1, s.length()));
 
@@ -40,19 +39,19 @@ public class CoinMachine {
     }
 
     public double check(int i) {
-        if (i == 50) {
-            return numberOf50;
-        }
-        return numberOf20;
+        return (i == 50) ? numberOf50 : numberOf20;
     }
 
-    public void getMoney(int i) { //i=140
-        if (i / 50 == 0) {
-            log.info("no 50 coins in the machine");
-        }
-        int takeFrom50 = i / 50; //2
-        numberOf50 -= takeFrom50; //98
+    public void getMoney(int i) {
+        int takeFrom50 = i / 50;
         int takeFrom20 = i % 50;
-        numberOf20 -= takeFrom20 / 20;
+        if (!(i / 50 == 0)) {
+            numberOf50 -= takeFrom50;
+            if (!(i / 20 == 0)) {
+                numberOf20 -= takeFrom20 / 20;
+            }
+            log.info("no more 20 coins in the machine ");
+        }
+        log.info("no 50 coins in the machine");
     }
 }
